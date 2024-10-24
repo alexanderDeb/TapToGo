9affb92eaf9dbf248a25db2b683369a2ca578b9a
TatianaAcosta22 <tatiacosta20032@gmail.com>
2024-10-24 16:53:23 -0500

    Version 1.0

--- src/Pages/register_page.jsx
+++ src/Pages/register_page.jsx
@@ -48,94 +48,115 @@ export default function Register() {
 
     };
     return (
-        <div className="min-h-screen flex items-center justify-center ">
-            {/* w-full ocupara todo el espacio horizontal del contenedor */}
-            {/* max-w-xl ancho máximo para el elemento */}
-            <div className="bg-blueForm p-8 rounded-lg shadow-lg w-full max-w-xl">
-                <p className="text-base-100 text-center font-bold mb-4">Ingresa los siguientes datos para registrar un administrador o usuario</p>
-                <form onSubmit={handlerSubmit} className="space-y-4">
-                    <div>
-                        <label className="block text-left text-base-100">Nombre</label>
-                        <input
-                            type="text"
-                            value={Name}
-                            onChange={(e) => setName(e.target.value)}
-                            required
-                            className="input input-bordered w-full"
-                        />
-                    </div>
-                    <div>
-                        <label className="block text-left text-base-100">Correo</label>
-                        <input
-                            type="email"
-                            value={Email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            required
-                            className="input input-bordered w-full"
-                        />
-                    </div>
-                    <div className="flex space-x-4">
-                        <div className="w-full">
-                            <label className="block text-left text-base-100">Rol</label>
-                            <input
-                            type="text"
-                            value={Role}
-                            onChange={(e) => setRole(e.target.value)}
-                            required
-                            className="input input-bordered w-full"
-                            />  
+      <div className="min-h-screen flex items-center justify-center ">
+        {/* w-full ocupara todo el espacio horizontal del contenedor */}
+        {/* max-w-xl ancho máximo para el elemento */}
 
-                        </div>
-                            
-                        <div className="w-full">
-                            <label className="block text-left text-base-100">Estado</label>
-                            <input
-                            type="text"
-                            value={Status}
-                            onChange={(e) => setStatus(e.target.value)}
-                            required
-                            className="input input-bordered w-full"
-                            />
-
-                        </div>
-                        
-                    </div>
-                    {/* // flex space-x-4 para que ambos campos estén alineados horizontalmente. */}
-                    <div className="flex space-x-4">
-                        <div className="w-full">
-                            <label className="block text-left text-base-100">Contraseña</label>
-                            <input
-                            type="password"
-                            value={Password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                            // w-full en Tailwind CSS se utiliza para establecer el ancho completo de un elemento
-                            className="input input-bordered w-full"
-                            />
-                        </div>
-
-                        <div className="w-full">
-                            <label className="block text-left text-base-100">Repetir contraseña</label>
-                            <input
-                            type="password"
-                            value={ConfirmPassword}
-                            onChange={(e) => setConfirmPassword(e.target.value)}
-                            required
-                            className="input input-bordered w-full"
-                            />
-
-                        </div>
-                        
-                    </div>
-                    <div className="text-center mt-6">
-                        <button type="submit" className="btn-circle w-full  bg-BTN hover:bg-BTNHover text-base-100 font-bold">
-                            Registrar
-                        </button>
-                    </div>
-                </form>
+        <div className="bg-blueForm p-8 rounded-lg shadow-lg w-full max-w-xl">
+          <p className="text-base-100 text-center font-bold mb-4">
+            Ingresa los siguientes datos para registrar un administrador o
+            usuario
+          </p>
+          <form onSubmit={handlerSubmit} className="space-y-4">
+            <div>
+              <label className="block text-left text-base-100">Nombre</label>
+              <input
+                type="text"
+                value={Name}
+                //onChange: Este es un evento que se dispara cada vez que el valor de un elemento de formulario cambia
+                // setName es una función de actualización de estado proporcionada por el hook useState
+                //e.target.value obtiene el valor actual del campo de entrada
+                onChange={(e) => setName(e.target.value)}
+                required
+                className="input input-bordered w-full"
+              />
             </div>
-        </div>
 
+            <div>
+              <label className="block text-left text-base-100">Correo</label>
+              <input
+                type="email"
+                value={Email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                className="input input-bordered w-full"
+              />
+            </div>
+
+            <div className="flex space-x-4">
+              <div className="w-full">
+                <label className="block text-left text-base-100">Rol</label>
+                <select
+                  value={Role}
+                  onChange={(e) => setRole(e.target.value)}
+                  required
+                  className="input input-bordered w-full"
+                >
+                  <option value="" disabled>
+                    Selecciona un rol
+                  </option>
+                  <option value="Usuario">Usuario</option>
+                  <option value="Administrador">Administrador</option>
+                </select>
+              </div>
+
+              <div className="w-full">
+                <label className="block text-left text-base-100">Estado</label>
+                <select
+                  value={Status}
+                  onChange={(e) => setStatus(e.target.value)}
+                  required
+                  className="input input-bordered w-full"
+                >
+                  <option value="" disabled>
+                    Selecciona un estado
+                  </option>
+                  <option value="Activo">Activo</option>
+                  <option value="Inactivo">Inactivo</option>
+                </select>
+              </div>
+            </div>
+            
+            {/* // flex space-x-4 para que ambos campos estén alineados horizontalmente. */}
+            <div className="flex space-x-4">
+              <div className="w-full">
+                <label className="block text-left text-base-100">
+                  Contraseña
+                </label>
+                <input
+                  type="password"
+                  value={Password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                  // w-full en Tailwind CSS se utiliza para establecer el ancho completo de un elemento
+                  className="input input-bordered w-full"
+                />
+              </div>
+
+              <div className="w-full">
+                <label className="block text-left text-base-100">
+                  Repetir contraseña
+                </label>
+                <input
+                  type="password"
+                  value={ConfirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  required
+                  className="input input-bordered w-full"
+                />
+              </div>
+            </div>
+            <div className="text-center mt-6">
+              <button
+                type="submit"
+                className="btn-circle w-full  bg-BTN hover:bg-BTNHover text-base-100 font-bold"
+              >
+                Registrar
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     );
 
 
